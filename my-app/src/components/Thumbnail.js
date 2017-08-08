6a169035f8332d31747bae367ba8d0f901b6f324
WILL MEIER <willmeierart@gmail.com>
2017-08-07 21:34:51 -0600

    yet another thumbnail refactor

--- my-app/src/components/Thumbnail.js
+++ my-app/src/components/Thumbnail.js
@@ -8,43 +8,24 @@ export default class Thumbnail extends Component {
   componentDidMount(){
     const {canvas} = this.refs
     const context = canvas.getContext('2d')
-    if (this.props.data.length > 0){
-      this.makeThumbnail(context)
+
+    this.makeThumbnail(context)
+
+  }
+  converter(matrix){
+    if (matrix.length > 0){
+      return matrix.map((each,index)=>{
+        const rgb = each.backgroundColor.match(/\d+/g)
+        const r = parseInt(rgb[0])
+        const g = parseInt(rgb[1])
+        const b = parseInt(rgb[2])
+        return {r, g, b}
+      })
     }
   }
-
-  converter(matrix){
-    return matrix.map((each,index)=>{
-      const color = each.backgroundColor
-      const findMultiDimArray=(i)=>{
-        const RGBdata = {x:0,y:0}
-        if (i<64){
-          RGBdata.x=i
-          RGBdata.y=0
-        } else {
-          RGBdata.x= i %64
-          RGBdata.y = (i-RGBdata.x)/64
-        }
-        return RGBdata
-      }
-      const rgb = color.match(/\d+/g)
-      const i = index
-      const iX = findMultiDimArray(index).x
-      const iY = findMultiDimArray(index).y
-      const r = parseInt(rgb[0])
-      const g = parseInt(rgb[1])
-      const b = parseInt(rgb[2])
-
-      return {i, iX, iY, r, g, b}
-    })
-  }
-
-
-
   makeThumbnail(ctx2){
     const rgb = this.converter(this.props.data)
     const imgData = ctx2.createImageData(64,32)
-
     for (var i=0; i<imgData.data.length; i+=4){
       const i2 = i==0? 0 : i/4
       imgData.data[i] = rgb[i2].r
@@ -52,10 +33,8 @@ export default class Thumbnail extends Component {
       imgData.data[i+2] = rgb[i2].b
       imgData.data[i+3] = 255
     }
-
     ctx2.putImageData(imgData,0,0)
     ctx2.imageSmoothingEnabled=false
-
   }
   render() {
     return (
